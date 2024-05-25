389242dd27fd0e6a6fd0ac97173c75885abac677
Swarup Chanda <swarupchanda1963@gmail.com>
2024-05-25 23:57:04 +0530

    added

--- app.py
+++ app.py
@@ -22,11 +22,14 @@ if uploaded_file is not None:
     img_arr = tf.keras.utils.img_to_array(image)
     img_data = np.expand_dims(img_arr, axis=0)
 
+try:
     predict = model.predict(img_data)
     score = tf.nn.softmax(predict)
 
     st.image(uploaded_file, width=200)
     st.subheader('Prediction Result')
     st.write(f'The Image is a {data_cat[np.argmax(score)]} with an accuracy of {np.max(score)*100:.2f}%')
+except Exception as e:
+    st.error(f"Netwrok Error during prediction: {e}")
 
 
